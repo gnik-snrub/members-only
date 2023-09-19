2bd511c483db9fc7e0fab8949cdda2fb06e1ee84
Josiah Morris <josiahbmorris@hotmail.com>
2023-09-20 02:54:18 +0900

    Added formatted_timestamp virtual field to MessageSchema

--- models/message.js
+++ models/message.js
@@ -1,3 +1,5 @@
+const { DateTime } = require('luxon')
+
 const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
@@ -6,6 +8,10 @@ const MessageSchema = new Schema({
   content: { type: String, required: true, maxLength: 200 },
   timestamp: { type: Date, required: true },
   author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+})
+
+MessageSchema.virtual('formatted_timestamp').get(function() {
+  return DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATETIME_SHORT)
 })
 
 module.exports = mongoose.model('Message', MessageSchema)
